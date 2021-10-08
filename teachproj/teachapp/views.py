c439af0d0319ab5f75db71ccd0501bf579d790b7
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-10-08 22:04:57 +0530

    second commit

--- teachproj/teachapp/views.py
+++ teachproj/teachapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Course
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    courses=Course.objects.all()
+
+    return render(request,'home.html',context={'courses':courses})
