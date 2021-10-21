baf0a61e572af77b72a0b2abf4e5c363ae757e6a
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-10-21 20:27:37 +0530

    first commit

--- teachproj/static/teachapp/js/course_page.js
+++ teachproj/static/teachapp/js/course_page.js
@@ -5,16 +5,21 @@
   //   maintainRatio()
 
   // }
+ var video_list;
+
   document.onreadystatechange = function () {
     if (document.readyState == 'interactive') {
       player = document.getElementById('player')
+      video_list=document.getElementById('video_list')
+      // video_list.style.maxHeight
       maintainRatio()
     }
   }
   function maintainRatio() {
     var w = player.clientWidth
     var h = w * 9 / 16
-    player.height = h;
+    player.height = h+'px';
+    video_list.style.maxHeight=h;
 
   }
   window.onresize = maintainRatio
